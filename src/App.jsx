b5e18b004b012ab67298ea9cf26c365de77c4377
Your Name <you@example.com>
2026-05-21 13:35:51 -0300

    cambio a HashRouter en appsjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
 import Catalogo from "./pages/Catalogo";
 import Admin from "./pages/Admin";
@@ -7,12 +7,12 @@ import Admin from "./pages/Admin";
 export default function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/"       element={<Catalogo />} />
           <Route path="/admin"  element={<Admin />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AppProvider>
   );
 }
